d03f88556444e35f6a9cf6b31e9242064918c45a
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-06 19:51:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,12 @@
 def speak_to_grandma(something)
   if something.upcase == something
     if something == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+       "I LOVE YOU TOO PUMPKIN!"
     else
-      puts "NO, NOT SINCE 1938!"
+       "NO, NOT SINCE 1938!"
     end
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
 
   end
 
